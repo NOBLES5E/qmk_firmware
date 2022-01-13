746c9052a1c6bed5039752380d4ebfa770916fe8
Xiangru Lian <admin@mail.xrlian.com>
2022-01-13 09:52:30 -0500

    chore: update layout

--- keyboards/keebio/foldkb/keymaps/shawn/keymap.c
+++ keyboards/keebio/foldkb/keymaps/shawn/keymap.c
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_VOLD,          _______, _______, _______, _______, _______, _______,     _______, KC_0,    _______, _______, _______, _______
   ),
   [2] = LAYOUT(
-    KC_MUTE, KC_ESC,  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,  KC_F12,
-    KC_EQL,           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,
+    KC_MUTE, KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______,   KC_BSPC,
+    KC_GRV,           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,
     _______,          KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_SCLN, KC_QUOT,           KC_ENT,
     KC_PGUP,          KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______,
     KC_PGDN,          KC_LCTL, _______, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,      KC_SPC,  KC_SPC,  KC_RALT, KC_RGUI, _______, KC_RCTL
