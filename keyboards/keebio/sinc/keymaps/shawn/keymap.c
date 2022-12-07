0c806d364566b18bfaa7b69523be4dc2377e4e69
Shawn Lian <shawn@lemonvideo.com>
2022-12-07 05:14:08 +0000

    chore: refine shawn's sinc keymap

--- keyboards/keebio/sinc/keymaps/shawn/keymap.c
+++ keyboards/keebio/sinc/keymaps/shawn/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______,   MO(1), KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,           MO(1),   KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RGUI, KC_RCTL, MO(1)),
   [1] = LAYOUT_80_with_macro(
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    RGB_HUI, RGB_HUD, KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
+    RGB_HUI, RGB_HUD, KC_PAST, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS,  _______, _______, _______,
     RGB_SAI, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, _______,          _______, _______,
     RGB_TOG, RGB_MOD, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
